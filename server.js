c175cd32c97fe41b55cd868e47647a6c31b5a346
kernalx02 <naimitaha02@gmail.com>
2026-03-30 23:58:38 +0100

    fixed some bugs pt.10

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // --- CORS CONFIGURATION ---
 app.use(cors({
-    origin: ["https://epcoran.netlify.app", "http://localhost:3000"],
+    origin: ["https://clubtesting.netlify.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
